fb60d5774f3aa0448332885f33bfffe38557f61c
su57ks <samuray.ks@yandex.ru>
2026-04-18 19:56:00 +0300

    spliting and clearing links

--- DataCollector/parser.py
+++ DataCollector/parser.py
@@ -6,9 +6,11 @@ random = requests.get("https://gram.cerm.ru/?mod=spelling").content
 with codecs.open("random.html", "wb") as f:
     f.write(random)
 
-data = re.search(r"<div id=\"spelling_alphabetical_words_nav\">.+Я</a></div>", random.decode()) # re.findall(r"<a href=\"https://gram.cerm.ru/spelling/.+\">.</a>", 
+data = re.search(r"<div id=\"spelling_alphabetical_words_nav\">.+Я</a></div>", random.decode()) 
 
-data = data.group().replace('<div id=\"spelling_alphabetical_words_nav\">', "")
+links = data.group().replace('<div id=\"spelling_alphabetical_words_nav\">', "").split("</a>")[:-1]
 
-print(len(data))
-print(data)
+links = [link.replace('<a href="', "")[:-3] for link in links]
+
+print(len(links))
+print(links)
